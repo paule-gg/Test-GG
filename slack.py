8b22b976cb84c79ff413cce5e8bb58c1110300ce
paule-gg <paule.grangette9@yahoo.com>
2024-12-10 18:42:32 +0100

    OKK

--- slack.py
+++ slack.py
@@ -1,12 +1,8 @@
 # jira.teststan.com
 print('Hello World!')
-<<<<<<< HEAD
-print('OK')
-=======
 
 test_slack_key = "xoxb-163213806324-FGqsdMF8t18v2N7Oq4i99vs5"
 print('OK')
 print('LOL')
->>>>>>> 3c34099 (slack LOL)
 
 print('OKKKK')

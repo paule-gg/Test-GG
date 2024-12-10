792d0df24f45499c1182524d73973d52d493e424
paule-gg <paule.grangette9@yahoo.com>
2024-12-10 18:53:04 +0100

    confluence 3

--- confluence.py
+++ confluence.py
@@ -1,3 +1,6 @@
 # confluence.teststan.com
 print('LOL')
 test_slack_key = "xoxb-163413206326-FGqsdMF8t18v6N7Oq4i39vs5"
+
+# confluence.teststan.com
+test_slack_key = "xoxb-163413206326-FGqsdMF8t18v6N7Oq4i39vs5"

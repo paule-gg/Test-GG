633a58432f69e3869ed0151ca8d72648c4886df7
paule-gg <paule.grangette9@yahoo.com>
2024-12-11 12:32:41 +0100

    Test confluence 3

--- test3.py
+++ test3.py
@@ -1,4 +1,7 @@
+url = "confluence.teststan.com"
 test_slack_key = "xoxb-163413206326-FGqsdMF8t18v6N7Oq4i39vs5"
+
+#test_slack_key = "xoxb-163413206326-FGqsdMF8t18v6N7Oq4i39vs5"
 
 def use_confluence():
     print(test_slack_key)

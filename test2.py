e99bffa9f0dd203e5a64cebcdac54f7d57d0b1c8
paule-gg <paule.grangette9@yahoo.com>
2024-12-11 12:31:08 +0100

    Test slack 2

--- test2.py
+++ test2.py
@@ -1,4 +1,7 @@
+url = "jira.teststan.com"
 test_slack_key = "xoxb-163213806324-FGqsdMF8t18v2N7Oq4i99vs5"
+
+#test_slack_key = "xoxb-163213806324-FGqsdMF8t18v2N7Oq4i99vs5"
 
 def use_slack():
     print(test_slack_key)

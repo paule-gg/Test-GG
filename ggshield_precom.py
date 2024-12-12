60ff133a47433171d084ceaf6fbba2d1039205d1
paule-gg <paule.grangette9@yahoo.com>
2024-12-12 10:56:15 +0100

    Test ignored

--- ggshield_precom.py
+++ ggshield_precom.py
@@ -3,3 +3,4 @@
 test_slack_key = "xoxb-163213806324-FGqsdMF8t18v2N7Oq4i99vs6"
 
 print('Lets try')
+print('OK')
--- slack_3.py
+++ slack_3.py
@@ -1,5 +1,6 @@
 url = "jira.teststan.com"
 test_slack_key = "xoxb-163213806324-FGqsdMF8t18v2N7Oq4i99vs5"
+print('LOL')
 
 def use_slack():
     print(test_slack_key)

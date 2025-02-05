c8512572727af15ab7ac9904ecf3bcf6267e1328
paule-gg <paule.grangette9@yahoo.com>
2025-02-05 22:22:19 +0100

    secret

---
+++ secret.py
@@ -0,0 +1,6 @@
+{
+  "AccessKeyId": "ASIAIOSFODNN7EXAMPLE",
+  "SecretAccessKey": "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
+  "SessionToken": "FQoGZXIvYXdzELD//////////wEa...",
+  "Expiration": "2025-02-04T12:34:56Z"
+}
